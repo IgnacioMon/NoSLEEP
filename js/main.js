00d3e9c8914577357be146ba667124ec815c018b
Ignacito <chupsris@gmail.com>
2020-10-02 23:14:11 -0300

    Sitio con Sliders problema

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**  First  Slider **/
+$(document).ready(function() {
+ 
+  $(".slider-one").owlCarousel({
+      navigation : true, 
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+  });
+ 
+});
